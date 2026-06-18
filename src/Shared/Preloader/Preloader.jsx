5248ff22b0710c3548c8a0d6586b0859adffef2d
namvaau <hoainam.vaau@gmail.com>
2026-06-18 16:16:27 +0700

    SECONDARY COMMIT MESSAGE

--- src/Shared/Preloader/Preloader.jsx
+++ src/Shared/Preloader/Preloader.jsx
@@ -1,69 +1,48 @@
-import { useEffect } from 'react';
-import './preloader.css';
+import { useEffect } from "react";
+import "./preloader.css";
 
 const Preloader = () => {
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      document.querySelector('#loading-screen').style.opacity = 0;
-      setTimeout(() => {
-        document.querySelector('#loading-screen').remove();
-      }, 300);
-    }, 1000); // Adjust the duration as needed
+useEffect(() => {
+const timeout = setTimeout(() => {
+const loadingScreen = document.querySelector("#loading-screen");
 
-    return () => clearTimeout(timeout);
-  }, []);
+  if (loadingScreen) {
+    loadingScreen.style.opacity = 0;
 
-  return (
-    <div
-      className='loading-screen'
-      id='loading-screen'
-    >
-      <span className='bar top-bar'></span>
-      <span className='bar down-bar'></span>
-      <div className='animation-preloader'>
-        <div className='spinner'></div>
-        <div className='loader'></div>
-        <div className='txt-loading'>
-          <span
-            data-text-preloader='M'
-            className='letters-loading'
-          >
-            M
-          </span>
-          <span
-            data-text-preloader='E'
-            className='letters-loading'
-          >
-            E
-          </span>
-          <span
-            data-text-preloader='D'
-            className='letters-loading'
-          >
-            D
-          </span>
-          <span
-            data-text-preloader='I'
-            className='letters-loading'
-          >
-            I
-          </span>
-          <span
-            data-text-preloader='I'
-            className='letters-loading'
-          >
-            I
-          </span>
-          <span
-            data-text-preloader='C'
-            className='letters-loading'
-          >
-            C
-          </span>
-        </div>
-      </div>
+    setTimeout(() => {
+      loadingScreen.remove();
+    }, 300);
+  }
+}, 1000);
+
+return () => clearTimeout(timeout);
+
+}, []);
+
+const title = "ĐÔNG HIẾU";
+
+return ( <div className="loading-screen" id="loading-screen"> <span className="bar top-bar"></span> <span className="bar down-bar"></span>
+
+```
+  <div className="animation-preloader">
+    <div className="spinner"></div>
+    <div className="loader"></div>
+
+    <div className="txt-loading">
+      {title.split("").map((char, index) => (
+        <span
+          key={index}
+          data-text-preloader={char}
+          className="letters-loading"
+        >
+          {char === " " ? "\u00A0" : char}
+        </span>
+      ))}
     </div>
-  );
+  </div>
+</div>
+
+);
 };
 
 export default Preloader;
